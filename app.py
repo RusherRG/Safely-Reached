a8f03e142cfb925129857700284329373a0916fb
rusherrg <rushang101@gmail.com>
2019-02-02 23:18:39 +0530

    Avinash Proof

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-import requests,pprint
+import requests,pprint,math
 app = Flask(__name__,template_folder=".")
 key = 'AIzaSyCxb7VjOyG0RqazuliBkyZlP3h437hshkk'
 
@@ -35,11 +35,10 @@ def safety(paths):
     count = 0
     for route in paths:
         path = route['overview_path']
-        index.append([0 for i in range(len(path))])
         for i in range(0,len(path),5):
             lat = path[i]['lat']
             lng = path[i]['lng']
-            url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+str(lat)+","+str(lng)+"&radius=25&type="
+            url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+str(lat)+","+str(lng)+"&radius=100&type="
             for factor in factors:
                 url += str(factor)+","
             url = url[:-1]
@@ -49,11 +48,24 @@ def safety(paths):
             for place in places['results']:
                 for factor in place['types']:
                     if factor in factors:
-                        count += 1
-                        index[-1][i] += 1
                         Places.append(place)
                         break
-    print(count,len(Places),index)
+        index.append([0 for i in range(len(path))])
+        best = []
+        for i in range(len(path)):
+            l = []
+            for place in Places:
+                x = place['geometry']['location']['lat']
+                y = place['geometry']['location']['lng']
+                x1 = path[i]['lat']
+                y1 = path[i]['lng']
+                print(math.sqrt((x-x1)**2 + (y-y1)**2)*111319.444444)
+                if math.sqrt((x-x1)**2 + (y-y1)**2)*111319.444444 <= 20:
+                    l.append(place)
+                    index[-1][i] += 1
+            best.append(l)
+    print(index)
+    
     return 
 if __name__ == "__main__":
     app.run(use_reloader=False)
