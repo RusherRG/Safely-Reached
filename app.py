d3a4a9e4e62491a8bb48371376e8d469847e690a
Akshay Padte <akshaypadte99@gmail.com>
2019-02-02 18:05:40 +0530

    fgdfgf

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
-app = Flask(__name__)
+app = Flask(__name__,template_folder=".")
 
 @app.route('/')
 def test():
-    return "Hi"
+    return render_template('test.html')
     
 @app.route('/getRoutes', methods=['GET'])
 def getRoutes():
@@ -14,9 +14,18 @@ def getRoutes():
     source.replace(' ', '+')
     destination.replace(' ', '+')
     key = 'AIzaSyCxb7VjOyG0RqazuliBkyZlP3h437hshkk'
-    url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+source+'&destination='+destination+'&sensor=false&alternativeas=true&mode=driving&key='+key
+    url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+source+'&destination='+destination+'&sensor=false&alternatives=true&mode=driving&key='+key
     r = requests.get(url)
+    
     r = r.text
-    return r
+    d = eval(r)
+    d['routes']=d['routes'][0:1]
+    return jsonify(d)
+
+@app.route('/findsafety', methods=['POST'])
+def safety():
+    data = request.json
+    print(data)
+    return jsonify(data)
 if __name__ == "__main__":
     app.run(use_reloader=False)
