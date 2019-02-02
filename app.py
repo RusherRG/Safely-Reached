57911f41581e6bd4ebabf8aa43cc956202b18f37
Akshay Padte <akshaypadte99@gmail.com>
2019-02-02 14:48:33 +0530

    maps

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ def test():
     
 @app.route('/getRoutes', methods=['GET'])
 def getRoutes():
+    print(request.args)
     source = request.args.get('source')
     destination = request.args.get('destination')
-
     source.replace(' ', '+')
     destination.replace(' ', '+')
     key = 'AIzaSyCxb7VjOyG0RqazuliBkyZlP3h437hshkk'
     url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+source+'&destination='+destination+'&sensor=false&alternativeas=true&mode=driving&key='+key
     r = requests.get(url)
     r = r.text
-    return "hello"
-
-app.run()
+    return r
+if __name__ == "__main__":
+    app.run()
